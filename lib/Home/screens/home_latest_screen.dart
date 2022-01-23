3660b12395054aae81f4b8a15230de7560866ead
Yohane <yohane910@gmail.com>
2022-01-23 19:47:34 +0800

    gl test

--- lib/Home/screens/home_latest_screen.dart
+++ lib/Home/screens/home_latest_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 
 import './news_detail_screen.dart';
+import '../providers/news_items.dart';
 
 class HomeLatest extends StatefulWidget {
   HomeLatest({Key? key}) : super(key: key);
@@ -49,17 +50,17 @@ class _HomeLatestState extends State<HomeLatest>
 
   @override
   Widget build(BuildContext context) {
-    var videoImages = [
-      'https://media.discordapp.net/attachments/844483893683814423/896050391978999889/info2.jpg?width=1202&height=676',
-      'https://i1.hdslb.com/bfs/archive/416a3ee705b2ac544ff76f60af52f6655bf6eb42.jpg@320w_200h.jpg',
-      'https://i0.hdslb.com/bfs/archive/00573099a2c86569ea68380bb44ccf91b0ca8619.png@320w_200h.png',
-      'https://i2.hdslb.com/bfs/archive/c781bb917d148a18dc0981ee8a8abdead75fb95f.jpg@320w_200h.jpg',
-      'https://i0.hdslb.com/bfs/archive/515eb7f4ca826304eeadbd51e124e27ecbfd1e17.jpg@320w_200h.jpg',
-      'https://i1.hdslb.com/bfs/archive/e22ff91e9363d48f03d86ed00185bcda10181dee.jpg@320w_200h.jpg',
-      'https://i0.hdslb.com/bfs/archive/9eca86255ef1838886b4b5f1c0662d3e9f634410.jpg@320w_200h.jpg',
-      'https://i2.hdslb.com/bfs/archive/be9686d5bb15d4ce330f73ee6acd0303a9170b5b.jpg@320w_200h.jpg',
-      'https://i2.hdslb.com/bfs/archive/bdd67d993c31ac4a9bc5054367229bad40c4000a.jpg@320w_200h.jpg',
-      'https://i2.hdslb.com/bfs/archive/a90dd608483742898f02e45ec039097446cdcd56.jpg@320w_200h.jpg'
+    List<SingleNewsItem> saaaa = [
+      SingleNewsItem(thumbnailImageUrl: 'https://media.discordapp.net/attachments/844483893683814423/896050391978999889/info2.jpg?width=1202&height=676' ,),
+      SingleNewsItem(thumbnailImageUrl: 'https://i1.hdslb.com/bfs/archive/416a3ee705b2ac544ff76f60af52f6655bf6eb42.jpg@320w_200h.jpg'),
+      // 'https://i0.hdslb.com/bfs/archive/00573099a2c86569ea68380bb44ccf91b0ca8619.png@320w_200h.png',
+      // 'https://i2.hdslb.com/bfs/archive/c781bb917d148a18dc0981ee8a8abdead75fb95f.jpg@320w_200h.jpg',
+      // 'https://i0.hdslb.com/bfs/archive/515eb7f4ca826304eeadbd51e124e27ecbfd1e17.jpg@320w_200h.jpg',
+      // 'https://i1.hdslb.com/bfs/archive/e22ff91e9363d48f03d86ed00185bcda10181dee.jpg@320w_200h.jpg',
+      // 'https://i0.hdslb.com/bfs/archive/9eca86255ef1838886b4b5f1c0662d3e9f634410.jpg@320w_200h.jpg',
+      // 'https://i2.hdslb.com/bfs/archive/be9686d5bb15d4ce330f73ee6acd0303a9170b5b.jpg@320w_200h.jpg',
+      // 'https://i2.hdslb.com/bfs/archive/bdd67d993c31ac4a9bc5054367229bad40c4000a.jpg@320w_200h.jpg',
+      // 'https://i2.hdslb.com/bfs/archive/a90dd608483742898f02e45ec039097446cdcd56.jpg@320w_200h.jpg'
     ];
 
     var videoContents = [
@@ -109,6 +110,7 @@ class _HomeLatestState extends State<HomeLatest>
               padding: EdgeInsets.symmetric(horizontal: 5, vertical: 0),
 
               child: new ListView.builder(
+                itemCount: 2,
                 shrinkWrap: true,
                 physics: new NeverScrollableScrollPhysics(),
                 itemBuilder: (BuildContext context, int index) {
@@ -138,7 +140,7 @@ class _HomeLatestState extends State<HomeLatest>
                                     topLeft: Radius.circular(5.0),
                                     topRight: Radius.circular(5.0)),
                                 child: new Image.network(
-                                  videoImages[index],
+                                  saaaa[index].thumbnailImageUrl,
                                   width: 420,
                                   height: 320,
                                   fit: BoxFit.cover,
@@ -211,7 +213,7 @@ class _HomeLatestState extends State<HomeLatest>
                     ),
                   );
                 },
-                itemCount: 10,
+                
               ),
             );
           }
