84b7f2a7f8be3a61782d602a43ac7028aa85e8bb
Yohane <yohane910@gmail.com>
2022-01-23 19:46:09 +0800

    gl test

--- lib/Home/providers/news_items.dart
+++ lib/Home/providers/news_items.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 
 class SingleNewsItem {
-  final String id;
-  final DateTime uploadTime;
-  final String title;
-  final int likes;
-  final int views;
+  // final String id;
+  // final DateTime uploadTime;
+  // final String title;
+  // final int likes;
+  // final int views;
   final String thumbnailImageUrl;
-  final List<String> tags;
+  // final List<String> tags;
 
-  SingleNewsItem(
-    this.id,
-    this.uploadTime,
-    this.title,
-    this.likes,
-    this.views,
-    this.thumbnailImageUrl,
-    this.tags,
-  );
+  SingleNewsItem({
+    // required this.id,
+    // required this.uploadTime,
+    // required this.title,
+    // required this.likes,
+    // required this.views,
+    required this.thumbnailImageUrl,
+    // required this.tags,  
+    });
 }
-
 
 class NewsItems {
   List<SingleNewsItem> _newsItems=[];
@@ -28,3 +27,5 @@ class NewsItems {
     return [..._newsItems];
   }
 }
+
+
